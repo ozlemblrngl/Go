13f21ceabfe7dc49faa49bf3d8b0611bd2b19c6a
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-19 16:40:15 +0300

    feature: new language kickstart

---
+++ GoTutorial/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Merhaba")
+	fmt.Println("Dünya")
+}
+
+// go büyük küçük harf duyarlıdır ve kullanmadığımız bir import olursa çalışmaz hata verir.
